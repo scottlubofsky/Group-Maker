e1afc5d18c584d7c02eb7e52d8a7dd690b7663e4
scottlubofsky <101009004+scottlubofsky@users.noreply.github.com>
2022-10-19 20:20:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+function renderGroups() {
+    let element = document.getElementById('groups');
+    let numOfGroups = document.getElementById('number-of-groups');
+    for (var i = 1; i <= Number(numOfGroups.value); i++) {
+        // element.innerHTML += `<div class='individual-group'><p>Group ${i}</p></div<br>`;
+        element.innerHTML += `<div class='individual-group'>Group ${i}</div><br>`;
+    }
+}
+
+function checkIfGroupsAreClear() {
+    let element = document.getElementById('groups');
+    if (element.innerHTML === "") {
+        renderGroups();
+    } else {
+        element.innerHTML = "";
+        renderGroups();
+    }
+}
+
+function addBox() {
+    let elements = document.getElementsByClassName('name-box');
+    let data = [].map.call(elements, elem => elem.value);
+
+    let element = document.getElementById('response');
+    element.innerHTML += `<input type='text' class='name-box' value=''><br>`;
+
+    for (var i = 0; i < data.length; i++) {
+        elements[i].value = data[i];
+    }
+}
+
+function generateRandomNumber(roster) {
+    return Math.floor(Math.random() * roster.length);
+}
+
+function formRoster() {
+    let names = document.getElementsByClassName('name-box');
+    let roster = [];
+    for (var i = 0; i < names.length; i++) {
+        roster.push(names[i].value);
+    }
+    return roster;
+}
+
+function formGroups() {
+    let numOfGroups = Number(document.getElementById('number-of-groups').value);
+    let groups = [];
+    let roster = formRoster();
+
+    for (var i = 0; i < numOfGroups; i++) {
+        groups.push([]);
+    }
+
+    function groupBuild() {
+        groups.forEach(group => {
+            let random = generateRandomNumber(roster);
+            if (roster[random] === undefined) {
+                return
+            } else {
+                group.push(roster[random]);
+            }
+            roster.splice(random, 1);
+        });
+    }
+
+    if (numOfGroups === 0) {
+        alert('Please enter number of groups.')
+        return
+    }
+
+    while (roster.length > 0) {
+        groupBuild();
+    }
+    return groups;
+}
+
+function clearGroupField() {
+    let groupArr = document.getElementsByClassName('individual-group');
+    for (var i = 1; i <= groupArr.length; i++) {
+        groupArr[i].innerHTML = "";
+    }
+}
+
+function addNamesToGroup() {
+    let groupArr = document.getElementsByClassName('individual-group');
+    let groups = formGroups();
+
+    for (var i = 0; i < groups.length; i++) {
+        for (var j = 0; j < groups.length; j++) {
+            console.log(`Group ${i + 1}: ${groups[i][j]}`)
+            groupArr[i].innerHTML += `<p>${groups[i][j]}</p>`;
+        }
+    }
+}
+
+function testing() {
+    addNamesToGroup();
+}
